5d81be8e60806ec7b4d0bc531833ba8225dd04c7
iallyschinke <iallyschinke@gmail.com>
2026-05-12 10:34:02 -0300

    Aula 04 e 05

--- js/tarefas.js
+++ js/tarefas.js
@@ -1,1 +1,23 @@
-// tarefas.js
+// Este módulo é responsável por validar as tarefas antes de adiciona-las
+
+// Função para validar i texto da tarefa, tem que ter ao menos 3 caracteres
+export function validarTarefa(texto) {
+  if (texto.trim() === "") {
+    return {
+      valida: false,
+      mensagem: "A tarefa não pode estar vazia.",
+    };
+  }
+
+  if (texto.length < 3) {
+    return {
+      valida: false,
+      mensagem: "A tarefa deve ter ao menos 3 caracteres.",
+    };
+  }
+
+  return {
+    valida: true,
+    mensagem: "Tarefa válida.",
+  };
+}
